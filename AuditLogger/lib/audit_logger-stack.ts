afed29d45576fffcd604a26e93aabeee86bcbd70
Brian Winkers <bwinkers@gmail.com>
2020-10-11 00:15:05 -0700

    Cleanup packages

--- AuditLogger/lib/audit_logger-stack.ts
+++ AuditLogger/lib/audit_logger-stack.ts
@@ -8,19 +8,16 @@ export class AuditLoggerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AuditLoggerQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'AuditLoggerTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'LoggerHandler', {
+    const logger = new lambda.Function(this, 'LoggerHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'logger.handler'                // file is "hello", function is "handler"
+      handler: 'logger.handler'                // file is "logger", function is "handler"
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: logger
     });
   }
 }
