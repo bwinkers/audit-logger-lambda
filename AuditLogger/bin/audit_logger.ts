cf5bae7fe5ca30cfab7c0d4a38cb3ae20c7d2c95
Brian Winkers <bwinkers@gmail.com>
2020-10-11 00:11:51 -0700

    Upgrade to 1.67

--- AuditLogger/bin/audit_logger.ts
+++ AuditLogger/bin/audit_logger.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AuditLoggerStack } from '../lib/audit_logger-stack';
 
--- AuditLogger/lib/audit_logger-stack.ts
+++ AuditLogger/lib/audit_logger-stack.ts
@@ -1,9 +1,26 @@
+import * as sns from '@aws-cdk/aws-sns';
+import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 
 export class AuditLoggerStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'AuditLoggerQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    const topic = new sns.Topic(this, 'AuditLoggerTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'LoggerHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'logger.handler'                // file is "hello", function is "handler"
+    });
   }
 }
