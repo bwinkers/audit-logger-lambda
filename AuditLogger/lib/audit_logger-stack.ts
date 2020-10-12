bb26387158c499e5cce5eab85e756baa11629c93
Brian Winkers <bwinkers@gmail.com>
2020-10-11 19:34:48 -0700

    WiP

--- AuditLogger/lib/audit_logger-stack.ts
+++ AuditLogger/lib/audit_logger-stack.ts
@@ -1,26 +1,34 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import { CfnOutput } from "@aws-cdk/core";
 
 export class AuditLoggerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AuditLoggerQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'AuditLoggerTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-
-    // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'LoggerHandler', {
+    // Defines an AWS Lambda resource
+    const logger = new lambda.Function(this, 'LoggerHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'logger.handler'                // file is "hello", function is "handler"
+      handler: 'logger.handler'                // file is "logger", function is "handler"
+    });
+
+    // Defines our DynamoDB Resource
+    const table = new dynamodb.Table(this, 'Table', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Use on-demand billing mode
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
+    });
+
+    // Output values
+    new CfnOutput(this, "TableName", {
+      value: table.tableName,
+      exportName: 'AuditLogTableName',
+    });
+    new CfnOutput(this, "TableArn", {
+      value: table.tableArn,
+      exportName: 'AuditLogTableArn',
     });
   }
 }
